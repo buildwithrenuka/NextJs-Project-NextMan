24f010430d721df2df93c8706c6f60cb36f9923a
Renuka <renuka.khirwadkarr@gmail.com>
2026-06-07 09:30:50 +0530

    understanding use state

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import {useState} from "react"
 
 export default function Home() {
+
+  const[count,setCount]=useState(100)
   return (
    <div className="bg-red-500 text-white p-8">
       <h1 className="text-3xl font-bold">NextMan 🚀</h1>
-      <p className="text-lg">Apna khud ka Postman!</p>
+      <p className="text-lg">Count:{count}</p>
+      <button onClick={()=>setCount(count+1)}>increment</button>
+      <button onClick={()=>setCount(count-1)}>decrement</button>
+
     </div>
   );
 }
